c58d962ea3c67bd853e06bcb5417b76dc43b34b3
Theodore <teohaik@gmail.com>
2017-05-26 23:45:35 +0300

    added task scheduling with node-cron and logging with winston

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var web = require('most-web');
 var cron = require('node-cron');
-
+var winston = require('winston');
 /**
  * start application
  */
@@ -9,12 +9,30 @@ web.current.start({
     bind:process.env.IP || '127.0.0.1'
 });
 
-/*
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: '/logs/siteCheckerLog.log' })
+    ]
+});
 
-var merchantBalanceChecker = require('./tasks/merchant-balance-checker.js');
+var requestConfig = {
+    url:"/Root/checkSites.json",
+    method:"POST",
+    body:{
+        "sites":[
+            {url:"http://chaikalis.gr"},
+            {url:"http://english-superfast.com/elearning"},
+            {url:"http://english-superfast.com/elearning-arab"},
+            {url:"http://english-superfast.com/elearning-rus"},
+            {url:"http://english-superfast.comaaaa"}
+        ]
+    }
+}
 
-cron.schedule('1 30 4 * * *', function(){//κάθε 04:30:01 το πρωί, κάθε μέρα
-    merchantBalanceChecker.checkBalance(function(){
-        winston.log("info","Balance check cron process finished!");
+cron.schedule('* *30 * * * *', function(){ //every 15 minutes
+    web.current.executeRequest(requestConfig, function (error,resp) {
+        var res = JSON.parse(resp.body);
+        winston.info(res);
     });
-});*/
+});
